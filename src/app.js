1cdfbf54d462e2a728ce2a74e576ca7ee89dff76
Nikku Haarika <80406188+NikkuHaarika@users.noreply.github.com>
2021-07-01 17:16:14 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,11 +23,17 @@ const PLAYERS = [
 
 // Player Class
 class Player {
-    constructor(id, name, type) {
+    constructor(id, name, type,img) {
         // Create member variables and assign values
         // Type your code
+        this.id=id;
+        this.name=name;
+        this.type=type;
+        this.strength=this.getRandomStrength();
+        this.selected == false;
+        this.image = "images/super-" + (id + 1) + ".png";
+}
 
-    }
 
     // getting random strength
     getRandomStrength = () => {
@@ -38,7 +44,19 @@ class Player {
     view = () => {
         // Accumulate HTML template
         // Type your code here
-
+        let player = document.createElement('div');
+        player.classList.add('player');
+        player.setAttribute('data-id', this.id);
+        if (this.selected == true)
+            player.classList.add('selected');
+        let image = document.createElement('img');
+        image.setAttribute('src', this.image);
+        let name = document.createElement('div');
+        name.textContent = this.name;
+        let strength = document.createElement('div');
+        strength.textContent = this.strength;
+        strength.className = 'strength';
+        player.append(image, name, strength);
     
         return player;
     }
@@ -50,7 +68,16 @@ class Superwar {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
-
+    this.players = players.map((player, i) => {
+        let type = (i % 2 == 0) ? 'hero' : 'villain';
+        return new Player(i, player, type);
+    });
+    this.score = [0, 0];
+    Array.from(document.getElementsByClassName('team'))
+        .forEach(elem => elem
+            .addEventListener('click', (e) => {
+                this.handleSelection(e.target);
+            }));
     }
 
     // Display players in HTML
